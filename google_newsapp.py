49cdd3edd90c649ea350eeec088cafbd8d19a4fe
gangamaha <ganga.mahadevan5@gmail.com>
2016-08-01 17:01:07 -0400

    Update google_newsapp.py
    cleaner compatibility for Python 2.x and 3.x

--- google_newsapp.py
+++ google_newsapp.py
@@ -6,37 +6,29 @@ keyword = raw_input("What news are your currently interested!!").strip()
 
 # Checking for user input. Exiting if there is no input as nothing to search for
 if keyword is '':
-    print "No topic entered by user. Please enter a valid topic name when prompted. Currently Exiting!!"
+    print("No topic entered by user. Please enter a valid topic name when prompted. Currently Exiting!!")
     exit(0)
+
+version = 3
+my_python_version = sys.version_info[0]  # My python version is 2.7 so my_python_version will be 2
+if my_python_version < version:
+    import urllib2 as py_url  # import urllib2 for Python 2.x
+else:
+    import urllib.request as py_url  # import urllib.request for Python 3.x
 
 
 def download_links(url):
-    version = (3,0)
-    python_version = sys.version_info[0]
-    if python_version >= version:
-        import urllib.request
-        try:
-            headers = {}
-            headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
-            req = urllib.request.Request(url, headers=headers)
-            response = urllib.request.urlopen(req)
-            page = response.read()
-            return page
-        except:  # If there is some problem in reaching the url
-            print("Cannot reach google.com, please check your internet connection and try later!!")
-            exit(0)  # Gracefully exit
-    else:
-        import urllib2
-        try:
-            headers = {}
-            headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
-            req = urllib2.Request(url, headers=headers)
-            response = urllib2.urlopen(req)
-            page = response.read()
-            return page
-        except:  # If there is some problem in reaching the url
-            print "Cannot reach google.com, please check your internet connection and try later!!"
-            exit(0)  # Gracefully exit
+    try:
+        headers = {}
+        headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
+        req = py_url.Request(url, headers=headers)
+        response = py_url.urlopen(req)
+        page = response.read()
+        return page
+    except Exception as e:  # If there is some problem in reaching the url
+        print(str(e))
+        print("Cannot reach google.com, please check your internet connection and try later!!")
+        exit(0)  # Gracefully exit
 
 
 def get_next_link(raw_html):
@@ -48,7 +40,7 @@ def get_next_link(raw_html):
     else:
         start_line = raw_html.find("class=\"_rQb\"")
         start_content = raw_html.find('href=', start_line+1)
-        end_content = raw_html.find('\" target',start_content+1)
+        end_content = raw_html.find('\" target', start_content+1)
         link = str(raw_html[start_content+6:end_content])
         return link, end_content
 
@@ -75,7 +67,7 @@ links = []
 links = get_links(raw_html)
 # open the links in new tabs of default browser, in my case Google chrome
 limit_news = 10
-print "Processing your news request. Please wait, each news will be opened as new tab in your default browser"
+print("Processing your news request. Please wait, each news will be opened as new tab in your default browser")
 for link in links:
         if limit_news != 0:
             webbrowser.open(link, new=new)
